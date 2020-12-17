30e9d1e254fe16b1aba4be55744a983037ad0e56
Emil <emil2001garipov@gmail.com>
2020-12-18 02:59:47 +0300

    Hw10 vue js init

---
+++ hw10/src/components/Footer.vue
@@ -0,0 +1,20 @@
+<template>
+    <footer>
+        <a href="/">Codeforces</a> &copy; 2020 by Mike Mirzayanov
+      <p>
+          Posts: {{ postCount }}
+          Users: {{ userCount }}
+      </p>
+    </footer>
+</template>
+
+<script>
+export default {
+    name: "Footer",
+    props: ["postCount", "userCount"]
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ hw10/src/components/Middle.vue
@@ -0,0 +1,54 @@
+<template>
+    <div class="middle">
+        <Sidebar :posts="viewPosts"/>
+        <main>
+            <Index v-if="page === 'Index'"/>
+            <Enter v-if="page === 'Enter'"/>
+            <WritePost v-if="page === 'WritePost'"/>
+            <EditPost v-if="page === 'EditPost'"/>
+            <Register v-if="page === 'Register'"/>
+            <Users v-if="page === 'Users'" :users="this.users"/>
+        </main>
+    </div>
+</template>
+
+<script>
+import Sidebar from "@/components/sidebar/Sidebar";
+import Index from "@/components/middle/Index";
+import Enter from "@/components/middle/Enter";
+import WritePost from "@/components/middle/WritePost";
+import EditPost from "@/components/middle/EditPost";
+import Register from "@/components/middle/Register";
+import Users from "@/components/middle/Users"
+
+export default {
+    name: "Middle",
+    data: function () {
+        return {
+            page: "Index"
+        }
+    },
+    components: {
+        Register,
+        WritePost,
+        Enter,
+        Index,
+        Sidebar,
+        EditPost,
+        Users
+    },
+    props: ["posts", "users"],
+    computed: {
+        viewPosts: function () {
+            return Object.values(this.posts).sort((a, b) => b.id - a.id).slice(0, 2);
+        }
+    },
+    beforeCreate() {
+        this.$root.$on("onChangePage", (page) => this.page = page)
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
